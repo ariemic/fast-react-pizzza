ce75b43210e4cefe5cf122b02a5b5ab583b81bed
Ariel <arie.michalik@gmail.com>
2024-09-09 19:46:03 +0200

    handle errors with new react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Error from "./ui/Error";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
 
-//* to use data fatchin in new router (from version 6) we need to defing router as an object istead of classic way, as we did in worldwise project
+//* to use data fatching in new router (from version 6) we need to defing router as an object istead of classic way, as we did in worldwise project
+//* so now data and urls are untitled together (all in one place)
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        //* we fetch data using loader func from Menu component, it happens when we go to /memu url
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order /> },
+    ],
   },
-  {
-    path: "/menu",
-    element: <Menu />,
-  },
-  { path: "/cart", element: <Cart /> },
-  { path: "/order/new", element: <CreateOrder /> },
-  { path: "/order/:orderId", element: <Order /> },
 ]);
 
 function App() {
--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,15 +1,20 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useRouteError } from "react-router-dom";
 
-function NotFound() {
+function Error() {
   const navigate = useNavigate();
+  const error = useRouteError();
+  console.log(error);
 
   return (
     <div>
       <h1>Something went wrong 😢</h1>
-      <p>%MESSAGE%</p>
+      <p>{error.data || error.message}</p>
+
+      {/*//? why here we use nagivate instead of navigation */}
+      {/*//*navigation is giving back some object with loading states (look at App)  */}
       <button onClick={() => navigate(-1)}>&larr; Go back</button>
     </div>
   );
 }
 
-export default NotFound;
+export default Error;
