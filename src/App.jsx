67825005a6301e524025d3d6fe045e6564a365b6
Ariel <arie.michalik@gmail.com>
2024-09-09 18:59:18 +0200

    add routes with vers 6 router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+//* to use data fatchin in new router (from version 6) we need to defing router as an object istead of classic way, as we did in worldwise project
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  { path: "/cart", element: <Cart /> },
+  { path: "/order/new", element: <CreateOrder /> },
+  { path: "/order/:orderId", element: <Order /> },
+]);
+
 function App() {
-  return <div>siema</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
