7267dcceef507da2dfcd1ec8b52aa2b3c296fef4
Ariel <arie.michalik@gmail.com>
2024-09-11 10:44:02 +0200

    delete dummy data
    fake cart, fake pizza

--- src/features/cart/cartSlice.jsx
+++ src/features/cart/cartSlice.jsx
@@ -2,15 +2,6 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   cart: [],
-  // cart: [
-  //   {
-  //     pizzaId: 12,
-  //     name: "Mediterranean",
-  //     quantity: 2,
-  //     unitPrice: 16,
-  //     totalPrice: 32,
-  //   },
-  // ],
 };
 
 const cartSlice = createSlice({
@@ -55,5 +46,7 @@ export const getTotalCartQuantity = (state) =>
 
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((acc, curr) => acc + curr.totalPrice, 0);
+
+export const getCart = (state) => state.cart.cart;
 
 export default cartSlice.reducer;
